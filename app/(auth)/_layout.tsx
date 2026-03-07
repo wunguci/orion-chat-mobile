ff9cf2f569a0af98244c0c3aae7f1b2db938b50c
Hồ Quang Nhân <22715701.nhan@student.iuh.edu.vn>
2026-03-07 14:01:06 +0700

    feat: Add Forgot Password flow and Profile screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
         <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="login" />
             <Stack.Screen name="register" />
-            <Stack.Screen name="home" />
+            <Stack.Screen name="forgotPassword" />
         </Stack>
     );
 }
